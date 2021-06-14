cf2e730e9b318efe7c5cec785c75b63923bdbfa5
raj311001 <rajaman311001@gmail.com>
2021-06-14 19:34:51 +0530

    first commit

---
+++ fs-module.js
@@ -0,0 +1,5 @@
+const fs = require ('fs');
+fs.readFileSync
+fs.writeFileSync
+const first = fs.readFileSync('./content/first.txt','utf-8');
+const second = fs.readFileSync('./content/second.txt','utf-8');
